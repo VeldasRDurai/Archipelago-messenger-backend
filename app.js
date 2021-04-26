a324a8006d5ba2c4b14efb2c9cdca17dccb71aa4
Veldas R Durai <veldasrdurai72@gmail.com>
2021-04-26 23:39:26 +0530

    installed socket io and added many socket io events which is necessary

--- app.js
+++ app.js
@@ -4,20 +4,46 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
+const http = require('http');
+const socketio = require('socket.io');
 const mongoose = require("mongoose");
 
-var app = express();
+const app = express();
+const server = http.createServer(app);
+const io = socketio( server , {cors:{ origin:'*' }});
 
 mongoose.connect( "mongodb://localhost:27017/" + "pesalama" , 
 { useNewUrlParser:true , useUnifiedTopology: true} );
 
+const historySchema = new mongoose.Schema({
+  email: { type : String ,  required: [ true , " No name specified...!"  ] },
+  // lastSendBy : { type : String ,  required: [ true , " No sendBy specified...!"  ] } ,
+  // lastMsg : { type : String ,  required: [ true , " No msg specified...!"   ] },
+  // lastMsgTime : { type : Date ,required: [ true , " No date specified...!"  ] },
+  unRead  : { type : Number , default:0 }
+});
 const accountSchema = new mongoose.Schema ({
   email : { type : String , required: [ true , " No email specified...!"     ] },
   name  : { type : String , required: [ true , " No name specified...!"     ] },
   password : { type : String },
   refreshToken : { type : String } ,
+  history  : { type : [historySchema] } 
 });
-const users   = mongoose.model( "Users" , accountSchema );
+
+const chatSchema = new mongoose.Schema({
+  sendBy : { type : String ,  required: [ true , " No sendBy specified...!"  ] } ,
+  msg    : { type : String ,  required: [ true , " No msg specified...!"  ] } ,
+  // msgTime: { type : Date   ,  required: [ true , " No Date specified...!"  ] } ,
+  // read   : { type : Boolean , default: false },
+});
+
+const activeUser = new mongoose.Schema({
+  email: { type : String ,  required: [ true , " No name specified...!"  ] },
+  socketId : { type : String }
+});
+
+const users   = mongoose.model( 'Users' , accountSchema );
+const activeUsers = mongoose.model( 'ActiveUsers' , activeUser );
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -37,6 +63,56 @@ app.use('/log-in',logInRouter({ users : users}));
 app.use('/sign-up',signUpRouter({ users: users}));
 app.use('/', indexRouter({ users : users}));
 
+io.on('connection' , async ( socket ) => {
+  console.log( 'user connected' , socket.id );
+  socket.emit('connected');
+  socket.on('newUser', async (data) => {
+    try {
+      console.log(data);
+      let acknowledge = await activeUsers({ email:data.email , socketId:socket.id }).save(); 
+      console.log( acknowledge );
+    } catch (e){ console.log(e); }
+  });
+  socket.on( 'search', async ( data ) => { 
+    try{
+      const userlist = await users.find({ 'email' : new RegExp( data.searchText ,"i") } , [ "email" ] , {limit:25} );
+      socket.emit('searchResult' , userlist);
+    } catch(e){
+      console.log(e);
+    }
+  });
+  socket.on('createRoom', async (data) => {
+    console.log("create room");
+    let names = [ data.userData.email , data.with ].sort();
+    socket.join( names[0] + names[1] );
+    console.log( socket.rooms );
+    const activeUsersList = await activeUsers.find({ email : data.with });
+    console.log( activeUsersList );
+    activeUsersList.length !== 0 && 
+    activeUsersList.forEach( item => socket.broadcast.to(item.socketId).emit("invite",{ room : names[0] + names[1] }) );
+  });
+  socket.on('joinRoom', function(data) {
+    socket.join(data.room);
+    console.log( socket.rooms );
+  });
+  socket.on( 'sendMsg' , async (data) => {
+    console.log(data);
+    // const activeUsersList = await activeUsers.find({ email : data.with });
+    // console.log( activeUsersList );
+    // activeUsersList.length !== 0 && 
+    // activeUsersList.forEach( item => socket.broadcast.to(item.socketId).emit("reciveMsg",{  }) );
+  });
+  socket.on( 'disconnect' , async () => {
+    try {
+      console.log('user disconnected' , socket.id );
+      let acknowledge = await activeUsers.deleteOne({ socketId:socket.id }); 
+      console.log( acknowledge );
+    } catch (e){
+      console.log(e);
+    }
+  });
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -53,4 +129,4 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+module.exports = { app:app , server:server };
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-module.exports = ( obj ) => {
+module.exports = ( { users } ) => {
 
   const authenticationRouter = require("./authentication");
-  router.use('/' , authenticationRouter({users:obj.users}));
+  router.use('/' , authenticationRouter({users:users}));
 
   router.get('/', function(req, res, next) {
     // let x = req.cookies['accessToken'];
     // res.send("<h1> HOME PAGE </h1>" + "<h3>" + x + "</h3>");
     res.json( { email : req.email } );
   });
+
+
   return router;
 }
