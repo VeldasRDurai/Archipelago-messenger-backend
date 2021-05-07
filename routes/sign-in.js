3c677484fe30f6359a81d0ab026904e66e75f301
Veldas R Durai <veldasrdurai72@gmail.com>
2021-05-07 23:29:37 +0530

    corrected some error

--- routes/sign-in.js
+++ routes/sign-in.js
@@ -30,8 +30,8 @@ router.post( '/', async (req, res, next) => {
         }
         const accessToken  = jwt.sign({email:payload.email} , process.env.ACCESS_TOKEN_SECRET , {expiresIn:"15m"} );
         const refreshToken = jwt.sign({email:payload.email} , process.env.REFRESH_TOKEN_SECRET );
-        res.cookie( "accessToken" , accessToken  , { path:"/" ,  httpOnly:true , maxAge: 900000, sameSite:'none' , secure:true } );
-        res.cookie( "refreshToken", refreshToken , { path:"/" ,  httpOnly:true , sameSite:'none', secure:true } ); 
+        res.cookie( "accessToken" , accessToken  , { path:"/" ,  httpOnly:true , maxAge: 900000, sameSite:'strict' , secure:true } );
+        res.cookie( "refreshToken", refreshToken , { path:"/" ,  httpOnly:true , sameSite:'strict', secure:true } ); 
         res.send('success');
     } catch (e){ 
         console.log(e);
