c479f66ac2aebb48f645ad599a3f075c5c6f5c1b
Veldas R Durai <veldasrdurai72@gmail.com>
2021-05-07 19:04:27 +0530

    small changes due to deplying error

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors( { origin : "https://archipelago-messenger.herokuapp.com/" , credentials : true } )); 
+app.use(cors( { origin : "https://archipelago-messenger.herokuapp.com" , credentials : true } )); 
 
 
 const indexRouter = require('./routes/index');
