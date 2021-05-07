46bcf6d15c20c500e69fc5933088e6feeb1de50e
Veldas R Durai <veldasrdurai72@gmail.com>
2021-05-08 00:32:27 +0530

    corrected some error

--- routes/authentication.js
+++ routes/authentication.js
@@ -23,6 +23,7 @@ router.use('/', async ( req , res , next ) => {
             req.email = payload.email;
             next();
         } catch (e) {
+            console.log(e);
             res.status(401).send(e);
         } 
     }
