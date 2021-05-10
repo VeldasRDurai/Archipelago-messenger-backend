0d8f09dce71c878de9c2f62f0e6c231450116b00
Veldas R Durai <veldasrdurai72@gmail.com>
2021-05-10 18:52:31 +0530

    forgot to add producion domains

--- database/database.js
+++ database/database.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose");
 const { accountSchema } = require('./account-schema');
 const { activeUsersSchema } = require('./active-users-schema');
 
-// const databaseName = 'archipelago';
-// const password = process.env.DATABASE_PASSWORD;
-// mongoose.connect( `mongodb+srv://VeldasRDurai:${password}@cluster0.u80jr.mongodb.net/${databaseName}?retryWrites=true&w=majority`,
-mongoose.connect( "mongodb://localhost:27017/" + "archipelago" , 
+// mongoose.connect( "mongodb://localhost:27017/" + "archipelago" , 
+const databaseName = 'archipelago';
+const password = process.env.DATABASE_PASSWORD;
+mongoose.connect( `mongodb+srv://VeldasRDurai:${password}@cluster0.u80jr.mongodb.net/${databaseName}?retryWrites=true&w=majority`,
     { useNewUrlParser:true , useUnifiedTopology: true} );
 
 const users = mongoose.model( 'Users' , accountSchema );
